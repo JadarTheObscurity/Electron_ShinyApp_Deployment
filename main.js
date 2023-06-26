2a37d303ae03b936dd7eb5a27a3cd33231cc2f1e
JadarTheObscurity <liaw911114@gmail.com>
2023-06-26 21:08:13 +0800

    v1.0.1

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-<<<<<<< HEAD
-
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-=======
 const url = require('url')
 const port = "9191"
 const child = require('child_process');
@@ -123,7 +105,6 @@ function cleanUpApplication(){
 
   mainWindow = null
 
->>>>>>> 9a200bc67040375e29083bce71aae517b88398a5
 }
 
 // This method will be called when Electron has finished
@@ -132,13 +113,6 @@ function cleanUpApplication(){
 app.whenReady().then(() => {
   createWindow()
 
-<<<<<<< HEAD
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-=======
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   console.log('EVENT::window-all-closed')
@@ -150,7 +124,6 @@ app.on('window-all-closed', function () {
 
   cleanUpApplication()
 
->>>>>>> 9a200bc67040375e29083bce71aae517b88398a5
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
